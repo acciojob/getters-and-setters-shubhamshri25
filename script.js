4c3fa87d77ed3b00498617dd68023a4b54b1997d
shubhamshri25 <105149595+shubhamshri25@users.noreply.github.com>
2023-04-12 19:16:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Person {}
+class Person {
+	Constructo(name,age){
+	this.name=name;
+	this.age=age;
+	}
 
-class Student extends Person {}
+	get getName(name){
+		console.log(this.name);
+	}
 
-class Teacher extends Person {}
+	set setAge(age){
+		console.log(this.age);
+	}
+}
+
+class Student extends Person {
+	study(){
+		console.log(this.name + " is studying");
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(this.name+ " is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
